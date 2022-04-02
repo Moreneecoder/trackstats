7198aef4630824ffbfda18c834b2d2cbab9b9690
Morenikeji Bello <caeserselassie@gmail.com>
2022-04-02 16:59:16 +0100

    Add not_null constraint to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_154716) do
+ActiveRecord::Schema.define(version: 2022_04_02_155652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.text "image"
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "image", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
