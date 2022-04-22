e2ac5e2cbd508aa35fa5003f995f0aa8ddb4251c
Morenikeji Bello <caeserselassie@gmail.com>
2022-04-22 23:17:30 +0100

    Include ImageUploader::Attachment in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+    include ImageUploader::Attachment(:image)
+    
     validates :name, presence: true, uniqueness: {case_sensitive: false}
 
     has_many :prices, dependent: :destroy
